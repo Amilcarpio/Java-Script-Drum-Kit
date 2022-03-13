67a70b7b44df3650afb42e063be177fb05bdc374
Amilcar Pio <amilcar.pio@gmail.com>
2022-03-13 08:16:57 -0300

    Final project

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('keydown', function(e){
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     console.log(key)
@@ -6,4 +6,13 @@ window.addEventListener('keydown', function(e){
     audio.currentTime = 0; //retorna para o início
     audio.play();
     key.classList.add('playing');
-});
+};
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return; //pula caso não seja um transform
+    this.classList.remove('playing'); //remove o efeito de transição da
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
